25cb25f1c1c595c6b51e8bef556c90c8c8f6128c
Antony Laroche <antony@larochefamily.net>
2018-04-14 22:57:08 -0500

    finished part 1 of homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,90 @@
-var inquire = require("inquirer");
+require("dotenv").config();
+
+var inquirer = require("inquirer");
 const chalk = require("chalk");
+var mysql = require("mysql");
+var keys = require("./keys.js");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: keys.mysql.mysql_password,
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`)
+    offer();
+});
+
+function offer() {
+    connection.query("SELECT * FROM products", function (err, result) {
+        if (err) throw err;
+        console.log("ID" + " | " + "PRODUCT" + " | " + "PRICE" + " | ");
+        for (var i = 0; i < result.length; i++) {
+            console.log(result[i].item_id + " | " + result[i].product_name + " | " + result[i].price + " | ");
+        }
+        console.log("-----------------------------------");
+        userRequest(result);
+    });
+}
+
+function userRequest(result) {
+    var itemArrNumber = 0;
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the ID of the item you would like to buy?",
+            name: "purchaseID",
+            validate: function (input) {
+                var idTest = false;
+                for (var i = 0; i < result.length; i++) {
+                    if (parseInt(input) === parseInt(result[i].item_id)) {
+                        idTest = true;
+                        itemArrNumber = i;
+                    }
+                }
+                if (idTest === true) {
+                    return idTest;
+                } else {
+                    console.log("\n\nPlease choose from the list of IDs displayed\n")
+                }
+            }
+        },
+        {
+            type: "input",
+            message: "\nHow many woulld you like to buy?",
+            name: "quantity",
+            validate: function (input) {
+                if (parseInt(input) <= parseInt(result[itemArrNumber].stock_quantity)) {
+                    return true;
+                } else {
+                    console.log("\n\nWe don't have enough in stock, please pick a different quantity\n")
+                }
+            }
+        }]).then(function (answer) {
+            var quantityRemain = parseInt(result[itemArrNumber].stock_quantity) - parseInt(answer.quantity);
+            var purchaseValue = parseInt(answer.quantity) * parseFloat(result[itemArrNumber].price);
+            updateInventory(quantityRemain, answer.purchaseID);
+
+            console.log(`Thank you for your purchase, you owe ${purchaseValue} dollars.`)
+        })
+}
+
+function updateInventory(remains, item) {
+    var query = connection.query("UPDATE products SET ? WHERE ?", [
+        {
+            stock_quantity: remains
+        },
+        {
+            item_id: item
+        }
+    ], function (err, res) {
+        if (err) throw err;
+        console.log(`${res.affectedRows} product updated!\n`);
+    });
+    // console.log(query.sql);
+}
+
