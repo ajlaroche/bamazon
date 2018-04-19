96332221d021fe50890d85ab9c56302d5f8c6e8a
Antony Laroche <antony@larochefamily.net>
2018-04-19 10:03:59 -0500

    added product sales column to database

--- MySQL/bamazon_db.sql
+++ MySQL/bamazon_db.sql
@@ -9,7 +9,44 @@ item_id INTEGER(10) PRIMARY KEY AUTO_INCREMENT
 , department_name VARCHAR(30) NOT NULL
 , price FLOAT(10,2) NOT NULL
 , stock_quantity INTEGER(10) NOT NULL
+, product_sales FLOAT (10,2) DEFAULT 0
 );
+
+CREATE TABLE departments (
+department_id INTEGER(10) PRIMARY KEY AUTO_INCREMENT
+, department_name VARCHAR(30) NOT NULL
+, overhead_costs FLOAT(10,2)
+);
+
+INSERT INTO departments (
+department_name
+, overhead_costs
+)
+VALUES (
+'condiments'
+, 50
+),
+(
+'bread'
+, 30
+),
+(
+'drinks'
+, 50
+),
+(
+'paper products'
+,20
+),
+(
+'meats'
+, 70
+),
+(
+'snacks'
+, 25
+);
+
 
 INSERT INTO products (
 product_name
@@ -97,3 +134,4 @@ VALUES (
 );
 
 SELECT * FROM products;
+
